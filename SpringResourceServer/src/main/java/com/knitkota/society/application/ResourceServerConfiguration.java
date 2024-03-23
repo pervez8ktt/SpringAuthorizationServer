d504d2488ff1f0b496344789bb0ece84f89691ca
pervez <pervez8ktt@gmail.com>
2024-03-24 02:00:56 +0530

    call resource server API

--- SpringResourceServer/src/main/java/com/knitkota/society/application/ResourceServerConfiguration.java
+++ SpringResourceServer/src/main/java/com/knitkota/society/application/ResourceServerConfiguration.java
@@ -8,6 +8,9 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -15,11 +18,22 @@ public class ResourceServerConfiguration {
 
 	@Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.securityMatcher("/articles/**")
+        http.cors(Customizer.withDefaults()).securityMatcher("/articles/**")
           .authorizeHttpRequests(authorize -> authorize.anyRequest()
             .hasAuthority("SCOPE_profile"))
           .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
 	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		config.addAllowedOrigin("http://127.0.0.1:3000");
+		config.setAllowCredentials(true);
+		source.registerCorsConfiguration("/**", config);
+		return source;
+	}
 }
